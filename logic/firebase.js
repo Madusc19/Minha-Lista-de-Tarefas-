98c103a5d1d0dc24b85851b553cfee6857378ff2
Madusc19 <105113538+Madusc19@users.noreply.github.com>
2022-08-27 10:37:23 -0300

    lista de tarefas

---
+++ logic/firebase.js
@@ -0,0 +1,14 @@
+// insira suas credenciais aqui
+ const firebaseConfig = {
+    apiKey: "AIzaSyDAJ8LKMfRtCcOlOVC5jknAnh01j7Ez-LU",
+    authDomain: "to-do-list-madu.firebaseapp.com",
+    databaseURL: "https://to-do-list-madu-default-rtdb.firebaseio.com",
+    projectId: "to-do-list-madu",
+    storageBucket: "to-do-list-madu.appspot.com",
+    messagingSenderId: "1097758624362",
+    appId: "1:1097758624362:web:9e0d71d5b6835b489f5813"
+  
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
